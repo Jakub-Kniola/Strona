8c0155721376cc09874f18f87f60ddf8893d2fce
Jakub-Kniola <105104671+Jakub-Kniola@users.noreply.github.com>
2022-05-08 12:15:07 +0200

    Delete htdocs directory

--- htdocs/legacy/session_required.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    session_start();
-
-    if (!isset($_SESSION['user_id'])) { # Sesja jest wymagana (trzeba być zalogowanym), aby wejść na daną stronę.
-        header('Location: /login.php');
-        return;
-    }
