3ad3d4c02d615f19341129b667b2c2b2ed10c168
Jakub-Kniola <105104671+Jakub-Kniola@users.noreply.github.com>
2022-05-09 11:13:34 +0200

    Delete htdocs directory

--- htdocs/legacy_stary_kod/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    require_once 'session_required.php';
-
-    unset($_SESSION['user_id']);
-    echo 'Wylogowano!'; # Tutaj nalezy zaprogramować co wyświtlić lub gdzie przenieść uzytkownika jeśli wylogowanie się powodło.
